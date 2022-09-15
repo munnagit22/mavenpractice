cb39905ed6021b7fbf519a654ed9d0194d48be84
munna prajapati <munna.mca07@gmail.com>
2022-09-15 14:36:46 +0530

    ticket no 433 fixed

--- Student.java
+++ Student.java
@@ -19,5 +19,11 @@ class Student
          System.out.println("Student Name="+ name);
          System.out.pritnln("Address="+address);
       }
-
+     //ticket no-433
+       void calGrade(int totalMarks)
+       {
+        
+            totalMarks>440?System.out.println("A"):System.out.println("B")
+          
+        }
  }
