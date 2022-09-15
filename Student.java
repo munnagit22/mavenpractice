cbb4218988cd380eccf70a761851cf0bc6ae7e49
MUNNA IT <munna.web21@gmail.com>
2022-09-15 14:00:40 +0530

    student java file modify

--- Student.java
+++ Student.java
@@ -1,8 +1,14 @@
 class Student
 {
+        String name;
+        Sting address;
       public static void main(String arg[])
       {
-
+         
      }
+      public void getData()
+      {
+
+       }
 
  }
